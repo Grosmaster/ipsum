8446152d8033df34fcb80452ae4678b0cb7b5467
Vladislav Gavrilov <B4B-eST-7cB-JPU>
2019-10-14 23:26:53 +0400

    Add junit to dependencies, write test for DistributionCenter

---
+++ src/test/java/model/DistributionCenterTest.java
@@ -0,0 +1,68 @@
+package model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import tasks.Flow;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistributionCenterTest {
+
+    private static DistributionCenter distributionCenter;
+    private static Resource resource;
+
+    @BeforeAll
+    static void setup() {
+        distributionCenter = new DistributionCenter(2, 2, 1, LocalTime.of(8, 0), LocalTime.of(22, 0));
+        resource = new Resource(20, 60);
+    }
+
+    @Test
+    void oneOrder() {
+        Order[] orders = new Order[1];
+        orders[0] = new Order(1, 2, 50, LocalTime.of(8, 0), LocalTime.of(22, 0), LocalTime.of(0, 5), LocalTime.of(0, 5));
+        Flow flow = new Flow(distributionCenter, orders, resource);
+        LocalTime startTime = LocalTime.of(14, 59, 36);
+        LocalTime endTime = LocalTime.of(1, 0, 24);
+flow.print();
+        assertEquals(flow.getStart(), startTime);
+        assertEquals(flow.getEnd(), endTime);
+        System.out.println("Success");
+    }
+
+    @Test
+    void threeOrders() {
+        Order[] orders = new Order[3];
+        orders[0] = new Order(1, 2.0001, 2.0001, LocalTime.of(8, 0), LocalTime.of(22, 0), LocalTime.of(0, 5), LocalTime.of(0, 5));
+        orders[1] = new Order(1, 2.0001, 2.0002, LocalTime.of(8, 0), LocalTime.of(22, 0), LocalTime.of(0, 5), LocalTime.of(0, 5));
+        orders[2] = new Order(1, 2.0002, 2.0001, LocalTime.of(8, 0), LocalTime.of(22, 0), LocalTime.of(0, 5), LocalTime.of(0, 5));
+        Flow flow = new Flow(distributionCenter, orders, resource);
+        flow.setOrders(orders);
+        flow.calculate();
+
+        LocalTime startTime = LocalTime.of(8, 0);
+        LocalTime endTime = LocalTime.of(8, 30, 1);
+        assertEquals(flow.getStart(), startTime);
+        assertEquals(flow.getEnd(), endTime);
+        System.out.println("Success");
+    }
+
+    @Test
+    void threeOrdersTwo(){
+        Order[] orders = new Order[3];
+        orders[0] = new Order(1, 2.1, 2.1, LocalTime.of(9, 0), LocalTime.of(22, 0), LocalTime.of(0, 5), LocalTime.of(0, 5));
+        orders[1] = new Order(1, 2.2, 2.2, LocalTime.of(8, 0), LocalTime.of(22, 0), LocalTime.of(0, 5), LocalTime.of(0, 5));
+        orders[2] = new Order(1, 2.3, 2.3, LocalTime.of(10, 0), LocalTime.of(22, 0), LocalTime.of(0, 5), LocalTime.of(0, 5));
+        Flow flow = new Flow(distributionCenter, orders, resource);
+        flow.setOrders(orders);
+        flow.calculate();
+
+        LocalTime startTime = LocalTime.of(8, 29, 17);
+        LocalTime endTime = LocalTime.of(10, 52,10);
+        assertEquals(flow.getStart(), startTime);
+        assertEquals(flow.getEnd(), endTime);
+        System.out.println("Success");
+    }
+}
