4b2d892b0cf103f7a05828d78e5c920a09ae111f
Vladislav Gavrilov <B4B-eST-7cB-JPU>
2019-11-17 14:47:51 +0400

    Serialize deserialize order object rest api

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,6 +1,8 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -11,18 +13,24 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Order implements Comparable, Serializable {
 
     private String id;
     private int weight;
     private double latitude;
     private double longitude;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime startTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime endTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime shippingTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime dischargingTime;
 
-    public Order(int weight, double latitude, double longitude, LocalTime startTime, LocalTime endTime, LocalTime shippingTime, LocalTime dischargingTime) {
+    public Order(int weight, double latitude, double longitude, LocalTime startTime, LocalTime endTime,
+                 LocalTime shippingTime, LocalTime dischargingTime) {
         this.weight = weight;
         this.latitude = latitude;
         this.longitude = longitude;
--- src/main/java/rest/RestApiController.java
+++ src/main/java/rest/RestApiController.java
@@ -1,30 +1,41 @@
 package rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import model.Order;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class RestApiController {
 
-    private final static String CREATE = "create";
-    private final static String ORDER = "order";
+    private final static String SLASH = "/";
+    private final static String CREATE = "/create";
+    private final static String GET = "/get";
+    private final static String ORDER = "/order";
+    private final static String ALL = "all/";
 
     private ArrayList<Order> orders = new ArrayList<>();
 
-    @RequestMapping("/"+CREATE+"/"+ORDER)
-    public void createOrder(@RequestParam(value = "order")String jsonOrder, HttpServletResponse response) throws IOException {
+    @RequestMapping(value = CREATE+ORDER, method = RequestMethod.POST)
+    public void createOrder(@RequestBody String jsonOrder, HttpServletResponse response) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         Order order = objectMapper.readValue(jsonOrder, Order.class);
         orders.add(order);
         response.setStatus(HttpServletResponse.SC_CREATED);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = GET + ORDER, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Order> getOrders(){
+        System.out.println(orders.get(0));
+        return orders;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
