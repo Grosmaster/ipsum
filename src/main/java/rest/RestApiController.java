ba01ca48efb26fd587443c10d73d91b900c65c4e
Vladislav Gavrilov <B4B-eST-7cB-JPU>
2019-11-18 00:17:41 +0400

    dummy commit

--- src/main/java/rest/RestApiController.java
+++ src/main/java/rest/RestApiController.java
@@ -8,34 +8,38 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
 
 @RestController
 public class RestApiController {
 
-    private final static String SLASH = "/";
     private final static String CREATE = "/create";
     private final static String GET = "/get";
+    private final static String ORDERS = "/orders";
     private final static String ORDER = "/order";
-    private final static String ALL = "all/";
 
-    private ArrayList<Order> orders = new ArrayList<>();
+    private HashMap<String, Order> orders = new HashMap<>();
 
     @RequestMapping(value = CREATE+ORDER, method = RequestMethod.POST)
     public void createOrder(@RequestBody String jsonOrder, HttpServletResponse response) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         Order order = objectMapper.readValue(jsonOrder, Order.class);
-        orders.add(order);
+        orders.put(order.getId(), order);
         response.setStatus(HttpServletResponse.SC_CREATED);
     }
 
     @ResponseBody
+    @RequestMapping(value = GET + ORDERS, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Order> getOrders(){
+        return orders.values();
+    }
+
+    @ResponseBody
     @RequestMapping(value = GET + ORDER, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Order> getOrders(){
-        System.out.println(orders.get(0));
-        return orders;
+    public Order getOrder(@RequestParam(value = "orderId") String orderId){
+        return orders.get(orderId);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
